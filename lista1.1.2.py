91c034aa38b44a34efc3b671b5531c8dba1687bf
Arthur Kafrouni Tomazi <arthur.k.t@hotmail.com>
2023-06-23 16:43:25 -0300

    Add files via upload

---
+++ lista1.1.2.py
@@ -0,0 +1,23 @@
+"""
+Lista 1, exercício 2
+
+Descrição: Este programa apresenta a resolução de uma questão da lista 1 da cadeira de Economia Computacional
+Autor:Arthur Kafrouni Tomazi
+Data: 23/06/2023
+Versão: 0.0.1
+"""
+
+# Atribuição de variáveis
+
+x=2
+y=3
+
+# Entrada de dados
+
+
+# Processamento de dados
+
+
+# Saída de dados
+
+print(y+x)
